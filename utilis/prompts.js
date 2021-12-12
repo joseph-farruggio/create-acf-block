9d16bae01211bd61a225d36b5344ad1f473314f6
Joseph Farruggio <joey@josephallendesign.com>
2021-12-12 17:00:14 -0600

    adds support for passing flags to filter prompts

--- utilis/prompts.js
+++ utilis/prompts.js
@@ -1,43 +1,73 @@
 const readlineSync = require('readline-sync');
 const { default: chalk } = require('chalk');
+const args = process.argv.slice(2);
 
 module.exports = () => {
-    let name = readlineSync.question(`
+    /** 
+     * If no args, show all prompts. 
+     * Otherwise only show required prompts and prompts for present flags
+     */
+    function allowPrompt(flag) {
+        if ( args.length === 0 ) {
+            return true;
+        } else if ( args.indexOf('--simple') != -1 ) {
+            return false;
+        } else {
+            return (args.indexOf(flag) != -1);
+        }
+    }
+
+    const prompt = {};
+    
+    prompt.name = readlineSync.question(`
     ${chalk.bold('Block name:')}
     ${chalk.dim('(String) A unique name that identifies the block (without namespace). For example ‘testimonial’. Note: A block name can only contain lowercase alphanumeric characters and dashes, and must begin with a letter.')}
     `);
-
-    let title = readlineSync.question(`
+    
+    prompt.title = readlineSync.question(`
     ${chalk.bold('Block title:')}
     ${chalk.dim('(String) The display title for your block. For example ‘Testimonial’.')}
     `);
+    
+    if (allowPrompt('--description')) {
+        prompt.description = readlineSync.question(`
+        ${chalk.bold('Block description:')}
+        ${chalk.dim('(String) (Optional) This is a short description for your block.')}
+        `);
+    }
 
-    let description = readlineSync.question(`
-    ${chalk.bold('Block description:')}
-    ${chalk.dim('(String) (Optional) This is a short description for your block.')}
-    `);
+    if (allowPrompt('--jsx')) {
+        prompt.jsx = readlineSync.keyInYN(`${chalk.bold('Use inner blocks:')} `);
+    }
 
-    let category = readlineSync.question(`
-    ${chalk.bold('Block category:')}
-    ${chalk.dim('(String) Blocks are grouped into categories to help users browse and discover them. The core provided categories are [ common | formatting | layout | widgets | embed ]. Plugins and Themes can also register custom block categories.')}
-    `);
+    if (allowPrompt('--category')) {
+        prompt.category = readlineSync.question(`
+        ${chalk.bold('Block category:')}
+        ${chalk.dim('(String) Blocks are grouped into categories to help users browse and discover them. The core provided categories are [ common | formatting | layout | widgets | embed ]. Plugins and Themes can also register custom block categories.')}
+        `);
+    }
 
-    let mode = readlineSync.question(`
-    ${chalk.bold('Block mode:')}
-    ${chalk.dim('(String) (Optional) The display mode for your block. Available settings are “auto”, “preview” and “edit”. Defaults to “preview”. auto: Preview is shown by default but changes to edit form when block is selected. preview: Preview is always shown. Edit form appears in sidebar when block is selected. edit: Edit form is always shown.')}
-    `);
+    if (allowPrompt('--mode')) {
+        prompt.mode = readlineSync.question(`
+        ${chalk.bold('Block mode:')}
+        ${chalk.dim('(String) (Optional) The display mode for your block. Available settings are “auto”, “preview” and “edit”. Defaults to “preview”. auto: Preview is shown by default but changes to edit form when block is selected. preview: Preview is always shown. Edit form appears in sidebar when block is selected. edit: Edit form is always shown.')}
+        `);
+    }
 
-    let align = readlineSync.question(`
-    ${chalk.bold('Block alignment:')}
-    ${chalk.dim('(String) (Optional) The default block alignment. Available settings are “left”, “center”, “right”, “wide” and “full”. Defaults to an empty string.')}
-    `);
+    if (allowPrompt('--align')) {
+        prompt.align = readlineSync.question(`
+        ${chalk.bold('Block alignment:')}
+        ${chalk.dim('(String) (Optional) The default block alignment. Available settings are “left”, “center”, “right”, “wide” and “full”. Defaults to an empty string.')}
+        `);
+    }
 
     return {
-       name: name, 
-       title: title, 
-       description: description,
-       category: category, 
-       mode: mode, 
-       align: align
+       name: prompt.name, 
+       title: prompt.title, 
+       description: prompt.description || '',
+       category: prompt.category || '',
+       mode: prompt.mode || '',
+       align: prompt.align || '',
+       jsx: prompt.jsx || '',
     };
 };
