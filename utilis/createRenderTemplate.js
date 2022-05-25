b79ce3e46c0ffb3398fb753f263a7d1a13f19202
Joey Farruggio <joey@joeyfarruggio.com>
2022-05-25 09:45:03 -0500

    update renderTemplate to follow ACF's example.

--- utilis/createRenderTemplate.js
+++ utilis/createRenderTemplate.js
@@ -1,31 +1,32 @@
-import fs from 'fs';
-import Conf from 'conf';
+import fs from "fs";
+import Conf from "conf";
 const config = new Conf();
-import Path from 'path';
+import Path from "path";
 
 function writeFile(path, contents, cb) {
-  fs.mkdir(Path.dirname(path), { recursive: true}, function (err) {
-    if (err) {
-      return cb(err);
-    }
+	fs.mkdir(Path.dirname(path), { recursive: true }, function (err) {
+		if (err) {
+			return cb(err);
+		}
 
-    fs.writeFile(path, contents, cb);
-  });
+		fs.writeFile(path, contents, cb);
+	});
 }
 
 export default (responses) => {
-  // Optional JSX
-  const innerBlocks = (responses.jsx) ? `<InnerBlocks allowedBlocks="' . esc_attr( wp_json_encode( $allowed_blocks ) ) . '" template="' . esc_attr( wp_json_encode( $template ) ) . '"/>'` : '';
-  const props = (responses.jsx) ? 
-    `$allowed_blocks = array( 'core/image', 'core/paragraph' );
+	// Optional JSX
+	const innerBlocks = responses.jsx ? `<InnerBlocks allowedBlocks="' . esc_attr( wp_json_encode( $allowed_blocks ) ) . '" template="' . esc_attr( wp_json_encode( $template ) ) . '"/>'` : "";
+	const props = responses.jsx
+		? `$allowed_blocks = array( 'core/image', 'core/paragraph' );
     $template = array(
       array( 'core/paragraph', array(
         'placeholder' => 'Add a root-level paragraph',
       ) 
-    ));` : '';
+    ));`
+		: "";
 
-  // Block render template content
-  const renderTemplateContent = `
+	// Block render template content
+	const renderTemplateContent = `
   <?php
   /**
    * Block Name: ${responses.title}
@@ -35,36 +36,40 @@ export default (responses) => {
   
   // Dynamic block ID
   $block_id = '${responses.name}-' . $block['id'];
+  if( !empty($block['anchor']) ) {
+    $block_id = $block['anchor'];
+  }
   
-  $alignClass = ( !empty($block['align']) ) ? ' align' . $block['align'] : "";
   
-  $blockClasses = implode(' ', array( $alignClass, $visibility, $block['className'] ));
+  // Create class attribute allowing for custom "className" and "align" values.
+  $className = '${responses.name}';
+  if( !empty($block['className']) ) {
+      $className .= ' ' . $block['className'];
+  }
+  if( !empty($block['align']) ) {
+      $className .= ' align' . $block['align'];
+  }
+
   
   ?>
   
-  <div id="<?php echo $block_id; ?>" class="<?php echo $blockClasses; ?>">
+  <div id="<?php echo $block_id; ?>" class="<?php echo $className; ?>">
     ${innerBlocks}
   </div>
   `;
 
-  /**
-   * Question: 
-   * Should I be creating this directory if it does not exist or throw an error?
-  **/
-  
-  if (config.get('groupAssets') === true) {
-    writeFile(`${config.get('renderTemplateFolderPath')}/${responses.name}/block.php`, 
-      renderTemplateContent, 
-      function (err)  { 
-        if (err) throw err;
-      }
-    );
-  } else {
-    writeFile(`${config.get('renderTemplateFolderPath')}/${responses.name}.php`, 
-      renderTemplateContent, 
-      function (err)  { 
-        if (err) throw err;
-      }
-    );
-  }
-}
+	/**
+	 * Question:
+	 * Should I be creating this directory if it does not exist or throw an error?
+	 **/
+
+	if (config.get("groupAssets") === true) {
+		writeFile(`${config.get("renderTemplateFolderPath")}/${responses.name}/block.php`, renderTemplateContent, function (err) {
+			if (err) throw err;
+		});
+	} else {
+		writeFile(`${config.get("renderTemplateFolderPath")}/${responses.name}.php`, renderTemplateContent, function (err) {
+			if (err) throw err;
+		});
+	}
+};
